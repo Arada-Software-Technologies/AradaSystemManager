6b76666fc7bd2db36f9791a4e7bb859a6eeb3c1b
lazyCoder-max <51710331+lazyCoder-max@users.noreply.github.com>
2021-05-24 14:16:31 +0300

    Starting Form Configured

--- AradaSystemManager/Program.cs
+++ AradaSystemManager/Program.cs
@@ -18,7 +18,7 @@ namespace AradaSystemManager
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdvertisementForm());
+            Application.Run(new Form1());
         }
     }
 }
