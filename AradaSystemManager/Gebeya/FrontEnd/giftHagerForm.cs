fd53ad7fdbc3c23ae9f00af1bc205e69f7f2d295
Leunama-g <47783627+Leunama-g@users.noreply.github.com>
2021-06-20 00:08:01 +0300

    finished hager gift form

---
+++ AradaSystemManager/Gebeya/FrontEnd/giftHagerForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AradaSystemManager.Gebeya.FrontEnd
+{
+    public partial class giftHagerForm : Form
+    {
+        public giftHagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
