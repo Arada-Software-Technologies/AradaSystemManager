846e1686c9135a5dc7ecc38381a3132a7c90deeb
Leunama-g <47783627+Leunama-g@users.noreply.github.com>
2021-06-19 20:53:07 +0300

    added hager management uc

---
+++ AradaSystemManager/Gebeya/FrontEnd/hagerMangement_UC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AradaSystemManager.Gebeya.FrontEnd
+{
+    public partial class cardMangement_UC : UserControl
+    {
+        public cardMangement_UC()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
